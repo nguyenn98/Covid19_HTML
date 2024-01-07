f06a653ed7525842f82db008f0bf343eaf4d5d52
nguyenn98 <nguyennguyenhvktqs98@gmail.com>
2024-01-07 22:01:37 +0700

    Covid-19 HTML

---
+++ covid.js
@@ -0,0 +1,4 @@
+var title_header = document.querySelector('#title-header');
+//console.log(title_header.getAttribute('id'));
+title_header.style = 'color: red';
+title_header.setAttribute("style", "color: white")
